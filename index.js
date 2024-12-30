2ad471ac7b4a1c8ce7fb3d56b19585f567ccd977
MogiliManikanta <you@example.com>
2024-12-30 18:38:34 +0530

    updated

--- index.js
+++ index.js
@@ -9,12 +9,31 @@ const salaryRouter = require("./routes/salary");
 const leaveRouter = require("./routes/leave");
 const settingRouter = require("./routes/setting");
 const dashboardRouter = require("./routes/dashboard");
+
+// Load environment variables
 dotEnv.config();
+
 const app = express();
-app.use(cors());
+
+// Configure CORS
+const corsOptions = {
+  origin: "https://employee-management-frontend-hazel.vercel.app", // Allow only your frontend origin
+  methods: "GET,POST,PUT,DELETE,OPTIONS", // Allowed methods
+  allowedHeaders: "Content-Type,Authorization", // Allowed headers
+  credentials: true, // Allow cookies/credentials
+};
+app.use(cors(corsOptions));
+
+// Parse JSON requests
 app.use(express.json());
+
+// Seed users (if needed)
 userRegister();
+
+// Serve static files
 app.use(express.static("public/uploads"));
+
+// API routes
 app.use("/api/auth", authRouter);
 app.use("/api/department", departmentRouter);
 app.use("/api/employee", employeeRouter);
@@ -22,6 +41,8 @@ app.use("/api/salary", salaryRouter);
 app.use("/api/leave", leaveRouter);
 app.use("/api/setting", settingRouter);
 app.use("/api/dashboard", dashboardRouter);
+
+// Start the server
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
 );
